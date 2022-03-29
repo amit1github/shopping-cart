f93fc54fa903516b7e84765ffa6774e790185695
Amit Yadav <samkr8991@gmail.com>
2022-03-30 00:46:27 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  const [cartItem, setCartItem] = useState([]);
+
+  const addInCart = (item) => {
+    const isAlreadyAdded = cartItem.findIndex(function (array) {
+      return array.id === item.id;
+    });
+
+    if (isAlreadyAdded !== -1) {
+      toast("already add in cart", {
+        type: "error",
+      });
+    }
+
+    setCartItem([...cartItem, item]);
+  };
+
+  const buyNow = () => {
+    setCartItem([])
+
+    toast("purchase complete", {
+      type: "success",
+    })
+  }
+
+  const removeItem = item => {
+    setCartItem(cartItem.filter(singleItem => singleItem.id !== item.id))
+  }
+
   return (
     <div className="App">
       <header className="App-header">
